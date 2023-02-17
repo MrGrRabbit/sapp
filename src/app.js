41dd79831067d9246222ce959a58c83066b15f1b
MrGrRabbit <58159733+MrGrRabbit@users.noreply.github.com>
2023-02-17 09:57:22 +0500

    trable in error.middleware&app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 const { UserController } = require('./components/user/user.controller');
 const AppMiddleware = require('./components/common/middleware/error.middleware');
+console.log(AppMiddleware);
 
 app.use('/user', new UserController().router);
-/*app.use((error, req, res, next) => {
-    console.log('Лог Еррор' + UserMiddleware.handleHttpError(error));
-    res.send(UserMiddleware.handleHttpError(error);
-});*/
-app.use((error, req, res, next) => {
-    const { code, message } = AppMiddleware.handleHttpError(error);
-    return res.status(code).send(message);
-});
+// Если res возвращать из error, то res - undefined
+app.use((error, req, res, next) => res.send(AppMiddleware(error)));
 
 module.exports = app;
